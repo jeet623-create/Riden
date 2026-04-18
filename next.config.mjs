b50b211d88ee9002bb901ca4cb70422abb575c63
jeet623-create <jeet623@gmail.com>
2026-04-18 10:45:11 +0530

    fix: ignore TS and ESLint errors at build time

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: { ignoreBuildErrors: true },
-    eslint: { ignoreDuringBuilds: true },
-    experimental: { missingSuspenseWithCSRBailout: false },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
+
 export default nextConfig
