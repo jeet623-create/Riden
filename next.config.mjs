7728d80f75edfb57d3019ae23ab60d797141e48b
jeet623-create <jeet623@gmail.com>
2026-04-18 10:29:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
+    eslint: { ignoreDuringBuilds: true },
+    experimental: { missingSuspenseWithCSRBailout: false },
 }
 export default nextConfig
